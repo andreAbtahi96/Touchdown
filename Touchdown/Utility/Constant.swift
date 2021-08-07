4a21345c142b9e2efb8fd0bfefaaf28a40b4ca76
andreAbtahi96 <sabtahi@uci.edu>
2021-08-07 01:48:02 -0700

    Created a footer, learned how to make code snippets, created a custom navigation bar, built animation for navigation bar, understood how to adapt to iPhone safe area.

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant .swift
+//  Touchdown
+//
+//  Created by Andre Abtahi on 8/7/21.
+//
+//  This file collects essential data: colors, fonts, etc into one file.
+
+import SwiftUI
+
+//  DATA
+
+//  COLOR
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+//  LAYOUT
+//  UX
+//  API
+//  IMAGE
+//  FONT
+//  STRING
+//  MISC
---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,41 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Andre Abtahi on 8/7/21.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    //  MARK: - PROPERTIES
+        
+    //  MARK: - BODY
+    var body: some View {
+        HStack(spacing: 4){
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+        }// HSTACK
+    }
+}
+
+//  MARK: - PREVIEW
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
